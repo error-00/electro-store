3fcb3b2cc1c88ef21b9aceebc45224e27aecc101
Kateryna Ihnatieva <ignatevaekaterina3@gmail.com>
2024-05-27 16:52:33 +0300

    added login and registration

--- electrostore/electrostore/urls.py
+++ electrostore/electrostore/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls", namespace="main")),
     path("goods/", include("goods.urls", namespace="goods")),
+    path("users/", include("users.urls", namespace="users")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- electrostore/users/views.py
+++ electrostore/users/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from main.models import Category
+from .forms import UserLoginForm, UserRegistrationForm
+from django.contrib import auth
 
-# Create your views here.
+
+def login(request):
+    categories = Category.objects.all()
+
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("main:index"))
+
+    context = {"title": "Login", "categories": categories}
+    return render(request, "users/login.html", context)
+
+
+def registration(request):
+    categories = Category.objects.all()
+
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserRegistrationForm()
+
+    context = {"title": "Sign In", "categories": categories}
+    return render(request, "users/registration.html", context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse("main:index"))
