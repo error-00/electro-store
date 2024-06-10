4951d4012edb7d0c037656db2d5ecc236bc1b54c
Kateryna Ihnatieva <ignatevaekaterina3@gmail.com>
2024-06-10 15:34:43 +0300

    added opportunity to change quantity of products in carts

--- electrostore/carts/views.py
+++ electrostore/carts/views.py
@@ -6,6 +6,12 @@ from .models import Cart
 
 def cart_add(request, product_slug):
     product = Product.objects.get(slug=product_slug)
+    try:
+        quantity = int(request.POST.get("quantity", 1))
+        if quantity < 1:
+            quantity = 1
+    except ValueError:
+        quantity = 1
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
@@ -13,10 +19,10 @@ def cart_add(request, product_slug):
         if carts.exists():
             cart = carts.first()
             if cart:
-                cart.quantity += 1
+                cart.quantity += quantity
                 cart.save()
         else:
-            Cart.objects.create(user=request.user, product=product, quantity=1)
+            Cart.objects.create(user=request.user, product=product, quantity=quantity)
 
     else:
         carts = Cart.objects.filter(
@@ -26,20 +32,27 @@ def cart_add(request, product_slug):
         if carts.exists():
             cart = carts.first()
             if cart:
-                cart.quantity += 1
+                cart.quantity += quantity
                 cart.save()
         else:
             Cart.objects.create(
-                session_key=request.session.session_key, product=product, quantity=1
+                session_key=request.session.session_key, product=product, quantity=quantity
             )
-    
+
     if not request.session.session_key:
         request.session.create()
 
     return redirect(request.META["HTTP_REFERER"])
 
 
-def cart_change(request): ...
+def cart_change(request, cart_id):
+    cart = Cart.objects.get(id=cart_id)
+    quantity = int(request.POST.get("quantity"))
+    if quantity > 0 and cart.quantity != quantity:
+        cart.quantity = quantity
+        cart.save()
+
+    return redirect(request.META["HTTP_REFERER"])
 
 
 def cart_remove(request, cart_id):
